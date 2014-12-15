7993f6fc711c09b17f64a72efd53c7232c43904a
ByteBlast <byteblast@gmail.com>
2014-12-15 20:45:27 +0000

    enhanced user experience.
    - pressing enter will now send the message.

--- Web/auxilium.js
+++ Web/auxilium.js
@@ -4,21 +4,22 @@
 
     auxilium.client.addMessage = function (username, message) {
         $("#chat").append(username + ": " + message);
-    };111
+    };
 
     $("#connectForm").submit(function (event) {
         event.preventDefault();
-
         $("#connectButton").attr("disabled", "disabled");
         $("#username").attr("disabled", "disabled");
         $.connection.hub.start().done(function () {
             $("#sendButton").removeAttr("disabled");
             $("#message").removeAttr("disabled");
             $("#message").focus();
-            $("#sendButton").click(function () {
+            $("#messageForm").submit(function (event) {
+                event.preventDefault();
                 auxilium.server.broadcast(
                     $("#username").val(),
                     $("#message").val() + "\r\n");
+                $("#message").val("");
             });
         });
     });
